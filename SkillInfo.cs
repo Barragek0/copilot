e16253427376e3972907a14583639a02116c773c
Barragek0 <barragek0lol@gmail.com>
2022-06-04 23:34:59 +0100

    Update SkillInfo.cs

--- SkillInfo.cs
+++ SkillInfo.cs
@@ -307,7 +307,7 @@ namespace CoPilot
                         break;
                     case "corrupting_fever":
                         focus.Id = skill.Id;
-                        focus.BuffName = "blood_surge_buff";
+                        focus.BuffName = "blood_surge";
                         break;
                 }
             }
